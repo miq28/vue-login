235a05e679f374a079094448e062573259ed42d7
ribonred <ribonred@gmail.com>
2021-12-12 15:53:48 +0700

    vue login

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,105 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="login">
+    <div class="login-header">
+      <h1>Login</h1>
+    </div>
+    <div class="login-body">
+      <form @submit.prevent="login">
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input type="text" class="form-control" id="username" v-model="username">
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" class="form-control" id="password" v-model="password">
+        </div>
+        <button type="submit" class="btn btn-primary">Login</button>
+      </form>
+      <button class="btn btn-primary" @click="showprofile">Show my profile</button>
+        <button type="submit" class="btn btn-primary" @click="logout" >Logout</button>
+
+      <div>
+        <p>{{ message }}</p>
+      </div>
+      <div v-if="profile">
+        <ul>
+          <li>your id : {{profile.id}}</li>
+          <li>your asset : {{profile.current_assets}}</li>
+          <li>your balance : {{profile.balance}}</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+const axios = require('axios');
+
+
 export default {
   name: 'HelloWorld',
+  data () {
+    return {
+      username: '',
+      password: '',
+      message:'you are not logged in',
+      token:'',
+      ref_token:'',
+      profile:false
+    }
+  },
   props: {
     msg: String
-  }
+  },
+  methods: {
+    login() {
+      const baseUrl='http://127.0.0.1:8000'
+      axios.post(`${baseUrl}/api/auth/`, {
+        email: this.username,
+        password: this.password
+      }).then(res => {
+        if (res.status === 200) {
+          this.message = `You are logged in!`
+          this.token = res.data.access
+          this.ref_token = res.data.refresh
+        }
+      }).catch(err => {
+        console.log(err);
+      })
+    },
+
+    showprofile() {
+        const baseUrl='http://127.0.0.1:8000'
+     
+        axios.get(`${baseUrl}/api/user/me/`,{
+        headers:
+      {
+        Authorization: `Bearer ${this.token}`
+        }
+      
+      }).then(res =>{
+        this.profile = res.data
+   
+      }).catch(err =>{
+        console.log(err);
+      })
+
+      },
+      logout(){
+      const baseUrl='http://127.0.0.1:8000'
+      console.log(this.token,"TOKEN")
+      axios.post(`${baseUrl}/api/auth/revoke/`, {
+        token: this.ref_token
+      }).then(res => {
+        console.log(res.status)
+          this.message = `You are logged out!`
+        
+      }).catch(err => {
+        console.log(err.response.data);
+      })
+      }
+    }
+
 }
 </script>
 
