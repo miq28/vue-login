6e8c37ec520423731c959868f3a530f2e50438c6
miq28 <iqbal.tiro@gmail.com>
2021-12-19 04:04:09 +0700

    lots of update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/android-chrome-192x192.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,25 +7,47 @@
       <form @submit.prevent="login">
         <div class="form-group">
           <label for="username">Username</label>
-          <input type="text" class="form-control" id="username" v-model="username">
+          <input
+            type="text"
+            value="mod@mod.com"
+            class="form-control"
+            id="username"
+            v-model="username"
+          />
         </div>
         <div class="form-group">
           <label for="password">Password</label>
-          <input type="password" class="form-control" id="password" v-model="password">
+          <input
+            type="password"
+            value="mod"
+            class="form-control"
+            id="password"
+            v-model="password"
+          />
         </div>
         <button type="submit" class="btn btn-primary">Login</button>
       </form>
-      <button class="btn btn-primary" @click="showprofile">Show my profile</button>
-        <button type="submit" class="btn btn-primary" @click="logout" >Logout</button>
+      <button class="btn btn-primary" @click="showprofile">
+        Show my profile
+      </button>
+      <button type="submit" class="btn btn-primary" @click="logout">
+        Logout
+      </button>
 
       <div>
         <p>{{ message }}</p>
       </div>
       <div v-if="profile">
         <ul>
-          <li>your id : {{profile.id}}</li>
-          <li>your asset : {{profile.current_assets}}</li>
-          <li>your balance : {{profile.balance}}</li>
+          <li>
+            your ID : <b>{{ profile.user.id }} </b>
+          </li>
+          <li>
+            your Name : <b>{{ profile.user.first_name }} {{ profile.user.last_name }}</b>
+          </li>
+          <li>
+            your District ID : <b>{{ profile.user.district_id }}</b>
+          </li>
         </ul>
       </div>
     </div>
@@ -33,74 +55,94 @@
 </template>
 
 <script>
-const axios = require('axios');
-
+const axios = require("axios");
+// const got = require("./node_modules/got");
+// import got from '@/got'
 
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      username: '',
-      password: '',
-      message:'you are not logged in',
-      token:'',
-      ref_token:'',
-      profile:false
-    }
+      username: "",
+      password: "",
+      message: "you are not logged in",
+      token: "",
+      ref_token: "",
+      profile: false,
+    };
   },
   props: {
-    msg: String
+    msg: String,
   },
   methods: {
     login() {
-      const baseUrl='http://127.0.0.1:8000'
-      axios.post(`${baseUrl}/api/auth/`, {
-        email: this.username,
-        password: this.password
-      }).then(res => {
-        if (res.status === 200) {
-          this.message = `You are logged in!`
-          this.token = res.data.access
-          this.ref_token = res.data.refresh
-        }
-      }).catch(err => {
-        console.log(err);
-      })
+      const baseUrl = "http://localhost:8000";
+      // const baseUrl = "https://ptb1prod.herokuapp.com";
+
+      axios
+        .post(`${baseUrl}/api/users/auth/email/`, {
+          email: this.username,
+          password: this.password,
+        })
+        .then((res) => {
+          if (res.status === 200) {
+            this.message = `You are logged in!`;
+            this.token = res.data.accessToken;
+            // this.token = res.headers["x-token"];
+            this.ref_token = res.data.refreshToken;
+            console.log(res.data);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
 
+    // async login2() {
+    //   try {
+    //     const response = await got(
+    //       `${baseUrl}/api/users/auth/email/`,
+    //       { json: true }
+    //     );
+    //     console.log(response.body.name);
+    //   } catch (error) {
+    //     console.log(error.response.body);
+    //   }
+    // },
+
     showprofile() {
-        const baseUrl='http://127.0.0.1:8000'
-     
-        axios.get(`${baseUrl}/api/user/me/`,{
-        headers:
-      {
-        Authorization: `Bearer ${this.token}`
-        }
-      
-      }).then(res =>{
-        this.profile = res.data
-   
-      }).catch(err =>{
-        console.log(err);
-      })
+      const baseUrl = "http://localhost:8000";
 
-      },
-      logout(){
-      const baseUrl='http://127.0.0.1:8000'
-      console.log(this.token,"TOKEN")
-      axios.post(`${baseUrl}/api/auth/revoke/`, {
-        token: this.ref_token
-      }).then(res => {
-        console.log(res.status)
-          this.message = `You are logged out!`
-        
-      }).catch(err => {
-        console.log(err.response.data);
-      })
-      }
-    }
-
-}
+      axios
+        .get(`${baseUrl}/api/users/me/`, {
+          headers: {
+            Authorization: `Bearer ${this.token}`,
+          },
+        })
+        .then((res) => {
+          this.profile = res.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    logout() {
+      const baseUrl = "http://127.0.0.1:8000";
+      console.log(this.token, "TOKEN");
+      axios
+        .post(`${baseUrl}/api/auth/revoke/`, {
+          token: this.ref_token,
+        })
+        .then((res) => {
+          console.log(res.status);
+          this.message = `You are logged out!`;
+        })
+        .catch((err) => {
+          console.log(err.response.data);
+        });
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
